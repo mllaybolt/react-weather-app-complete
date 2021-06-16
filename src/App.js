8847d8660e95b04c3b3f8c10248d73fed95a41a6
mllaybolt <81402055+mllaybolt@users.noreply.github.com>
2021-06-15 23:41:43 -0500

    installed Bootstrap

--- src/App.js
+++ src/App.js
@@ -4,16 +4,18 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project was coded by Michelle Laybolt and is open-sourced on{" "}
-        <a
-          href="https://github.com/mllaybolt/react-weather-app-complete"
-          target="_blank"
-        >
-          GitHub
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          This project was coded by Michelle Laybolt and is open-sourced on{" "}
+          <a
+            href="https://github.com/mllaybolt/react-weather-app-complete"
+            target="_blank"
+          >
+            GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
