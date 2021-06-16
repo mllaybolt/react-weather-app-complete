c497466d4666ede1fb1a0e432fb4baa2982ae48a
mllaybolt <81402055+mllaybolt@users.noreply.github.com>
2021-06-15 23:27:15 -0500

    Added footer with GitHub repo URL

--- src/App.js
+++ src/App.js
@@ -2,5 +2,18 @@ import React from "react";
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        This project was coded by Michelle Laybolt and is open-sourced on{" "}
+        <a
+          href="https://github.com/mllaybolt/react-weather-app-complete"
+          target="_blank"
+        >
+          GitHub
+        </a>
+      </footer>
+    </div>
+  );
 }
