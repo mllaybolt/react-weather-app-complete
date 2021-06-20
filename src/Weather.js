a5cf97b399183564cff93e2346a020612e6f4ba6
mllaybolt <81402055+mllaybolt@users.noreply.github.com>
2021-06-19 21:30:41 -0500

    Added function for pulling lon + lat data from API through axios

--- src/Weather.js
+++ src/Weather.js
@@ -11,6 +11,7 @@ export default function Weather(props) {
     console.log(response.data);
     setWeatherData({
       ready: true,
+      coordinates: response.data.coord,
       temperature: Math.round(response.data.main.temp),
       wind: response.data.wind.speed,
       city: response.data.name,
@@ -60,7 +61,7 @@ export default function Weather(props) {
           </div>
         </form>
         <WeatherInfo data={weatherData} />
-        <WeatherForecast />
+        <WeatherForecast coordinates={weatherData.coordinates} />
       </div>
     );
   } else {
--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,12 +1,18 @@
 import React from "react";
 import WeatherIcon from "./WeatherIcon";
 import "./WeatherForecast.css";
+import axios from "axios";
 
-export default function WeatherForecast() {
-  let apiKey = "66e11cc33b0837aff2265fadb13ac0ad";
-  let longitude = 40.7;
-  let latitude = 74;
-  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}`;
+export default function WeatherForecast(props) {
+  function handleResponse(response) {
+    console.log(response.data);
+    let apiKey = "66e11cc33b0837aff2265fadb13ac0ad";
+    let longitude = props.coordinates.lon;
+    let latitude = props.coordinates.lat;
+    let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
+  }
+
+  axios.get(apiUrl).then(handleResponse);
 
   return (
     <div className="WeatherForecast">
